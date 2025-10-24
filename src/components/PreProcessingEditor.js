e4faa246ffcf34cb9f9446aaad14a87e5cc6a560
hirunihansimadabare <dabay006@mymail.umisa.edu.au>
2025-10-24 16:57:21 +1030

    Create PreProcessingEditor component

---
+++ src/components/PreProcessingEditor.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function PreProcessingEditor({text, onTextChange}){
+    const handleChange = (event) => {
+        onTextChange(event.target.value);
+    };
+
+    return (
+        <>
+            <label htmlFor="proc" className="form-label">Text to preprocess:</label>
+            <textarea className="form-control" rows="15" id="proc" value={text} onChange={handleChange}
+            ></textarea>
+        </>
+    )
+}
