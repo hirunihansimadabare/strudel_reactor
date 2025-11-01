c23540e3b93261026535877e097abf7113520743
hirunihansimadabare <dabay006@mymail.umisa.edu.au>
2025-11-01 14:48:49 +1030

    Setup D3 chart dimensions, SVG container and linear scales

--- src/components/D3Graph.js
+++ src/components/D3Graph.js
@@ -15,7 +15,24 @@ export default function D3Graph({height = 220}) {
             if (D3Container.current && data && data.length > 0){
                 //Clear if any previous graph elements
                 d3.select(D3Container.current).selectAll('*').remove();
+
+                //Set up dimensions
+                const margin = {top: 20, right: 20, bottom: 30, left: 50 };
+                const width = D3Container.current.clientWidth - margin.left - margin.right;
+                const height = D3Container.current.clientHeight - margin.top - margin.bottom;
+
+                //Append the SVG object
+                const svg = d3.select(D3Container.current).append("svg").attr("width", width + margin.left +margin.right)
+                .attr("height", height + margin.top, margin.bottom).append("g").attr("transform",`translate(${margin.left}, ${margin.top}`);
+
+                /* Scales */
+                //X scale
+                const x =  d3.scaleLinear().domain([0, data.length - 1]).range([height, 0]);
+
+                //Y scale
+                const y = d3.scaleLinear().domain([0, d3.max(data, d => d.value)]).range([height, 0]);
             }
+
         }
     })
 }
